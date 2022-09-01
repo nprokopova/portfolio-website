4346cc229837d331292d010e292226c5cfc6a659
nprokopova <n.prokopova96@gmail.com>
2022-09-01 13:44:54 +0300

    favicon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link href="/favicon.ico" rel="shortcut icon" />
+          <link href="/images/favicon.ico" rel="icon" type="image/png"/>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700;800;900&display=swap"
